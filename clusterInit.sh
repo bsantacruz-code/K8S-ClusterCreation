8bf713b815e1265da2e12e15d381278cd0645b00
Breider Alexander Santacruz <bsnthacruz@gmail.com>
2023-12-16 19:55:58 -0500

    Scripts created

---
+++ clusterInit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=1.27.0
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+
+kubectl get nodes
